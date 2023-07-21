51ab909f30f0e9365d3275a99a57073edf68c64c
Vitalii Fomenko <vfomenko@Vitaliis-MacBook-Pro-23068.local>
2023-07-21 14:44:30 +0300

    added routing for pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,23 @@
-import { useState } from 'react';
-import AppHeader from "../appHeader/AppHeader";
-import AppBanner from '../appBanner/AppBanner';
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from '../comicsList/ComicsList';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import decoration from '../../resources/img/vision.png';
+import AppHeader from "../appHeader/AppHeader";
+import {MainPage, ComicsPage} from '../pages';
+
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => setChar(id);
-
         return (
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={onCharSelected}/>
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                            <CharInfo charId={selectedChar}/>
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                    <AppBanner/>
-                    <ComicsList/>
-                </main>
-            </div>
+            <BrowserRouter>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>} />
+                            <Route path="/comics" element={<ComicsPage/>} />
+                        </Routes>
+                    </main>
+                </div>
+            </BrowserRouter>
         )
 
 }
